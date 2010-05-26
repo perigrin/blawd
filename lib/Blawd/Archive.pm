daee12db20d868ddce1c634a65f06f946041a967
franck cuny <franck@lumberjaph.net>
2010-05-26 20:44:25 +0200

    add separator

--- lib/Blawd/Archive.pm
+++ lib/Blawd/Archive.pm
@@ -4,17 +4,19 @@ extends 'Blawd::Index';
 
 sub render_page_HTML {
     my $self = shift;
-    return '<div class="index"><ul>' . (
+    return '<div class="index"><h1>Archives</h1><ul>' . (
         join "\n",
         map {
             "<li>"
               . $_->title
-              . ' <span class="timestamp">('
+              . ' &#8212; <span class="timestamp">'
               . $_->date
-              . ")</span></li>"
+              . "</span></li>"
           } $self->entries
     ) . "</ul></div>";
 }
+
+
 
 sub render_fragment_HTML {
     my $self = shift;
